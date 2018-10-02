ac649f04dc8f10ac2f28c78f15052bc9113fb43c
Chris Deister <cdeister@gmail.com>
2018-10-02 10:08:59 -0400

    Miller Effect warning in flyback opto documentation.

--- microcontrollerCode/simpRamp/simpRamp.ino
+++ microcontrollerCode/simpRamp/simpRamp.ino
@@ -5,14 +5,17 @@ elapsedMicros loopTime;
 bool pulseState = 0;
 bool pulseSwitched = 0;
 
-int lineTime = 200;
+int lineTime = 100;
 int flybackTime = 10;
 int rampInc = 4095 / lineTime;
 int downInc = 4095 / flybackTime;
 int stimAmp = 0;
+int gatePause = 2;
 
 void setup() {
   analogWriteResolution(12);
+  pinMode(23,OUTPUT);
+  digitalWrite(23,LOW);
 
 }
 
@@ -36,7 +39,16 @@ void loop() {
   analogWrite(A14, stimAmp);
   delayMicroseconds(1);
 
+  if ((stimAmp>=2000) && (gatePause==0)){
+    digitalWrite(23,HIGH);
+    delayMicroseconds(200);
+    digitalWrite(23,LOW);
+    gatePause=1;
+    loopTime=0;
+  }
 
-
+  if ((loopTime >=1000) && (gatePause==1)){
+    gatePause=0;
+  }
 
 }
